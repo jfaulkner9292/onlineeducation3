e20a1c88ac2482915a2cb9f7c8580f6b2914185e
jfaulkner9292 <jerrison9292@gmail.com>
2020-03-05 11:43:43 -0600

    everything

--- client/main.js
+++ client/main.js
@@ -26,7 +26,7 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
-import './registrationNlogin.html';
+//import './registrationNlogin.html';
 
 /*Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
